daa1a7449b63ac10e6cb7268ba8653385f9fb64a
yocmay <61123311+yocmay@users.noreply.github.com>
2021-02-09 20:02:57 +0200

    Update __init__.py
    - Changed timing from minutes to seconds. 
    - Changed the notification text to report target and current temperature.

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ class Cooling(StepBase):
     # Properties
     temp = Property.Number("Temperature", configurable=True, description="Target Temperature")
     kettle = StepProperty.Kettle("Kettle", description="Kettle in which the Chilling takes place")
-    timer = Property.Number("Timer in Minutes", configurable=True, description="Timer is started when the target temperature is reached")
+    timer = Property.Number("Timer in Seconds", configurable=True, description="Timer is started when the target temperature is reached")
 
     def init(self):
         '''
@@ -19,7 +19,7 @@ class Cooling(StepBase):
         '''
         # set target tep
         self.set_target_temp(self.temp, self.kettle)
-
+        
     @cbpi.action("Start Timer Now")
     def start(self):
         '''
@@ -28,28 +28,28 @@ class Cooling(StepBase):
         :return:
         '''
         if self.is_timer_finished() is None:
-            self.start_timer(int(self.timer) * 60)
-
+            self.start_timer(int(self.timer))
+            
     def reset(self):
         self.stop_timer()
         self.set_target_temp(self.temp, self.kettle)
-
+        
     def finish(self):
         self.set_target_temp(0, self.kettle)
-
+        
     def execute(self):
         '''
         This method is execute in an interval
         :return:
         '''
-
+        
         # Check if Target Temp is reached
         if self.get_kettle_temp(self.kettle) <= float(self.temp):
             # Check if Timer is Running
             if self.is_timer_finished() is None:
-                self.start_timer(int(self.timer) * 60)
-
+                self.start_timer(int(self.timer))
+                
         # Check if timer finished and go to next step
         if self.is_timer_finished() == True:
-            self.notify("Cooling Step Completed!", "Starting the next step", timeout=None)
+            self.notify("Cooling Step Completed! Target temp is: %s C" % (self.temp), "Current temp is: %s C" % (self.get_kettle_temp(self.kettle)), timeout=10000)
             self.next()
